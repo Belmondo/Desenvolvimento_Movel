e19e3925bbb66f94a245f64c7b2c72228e2f5189
Belmondo Rodrigues <belmondo.rodrigues@gmail.com>
2023-06-11 15:48:51 -0300

    Update question.dart
    [PT-BR] Aplicando Estilo ao Widget Question
    [ENG] Applying Style to the Question Widget

--- Flutter/primeiro_projeto/lib/question.dart
+++ Flutter/primeiro_projeto/lib/question.dart
@@ -9,6 +9,14 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(textQuestion);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(20),
+      child: Text(
+        textQuestion,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
